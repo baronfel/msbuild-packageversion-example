23ff8fa0b0eea5e52303935e0af49ce2d44132ed
Chet Husk <chusk3@gmail.com>
2022-12-09 07:30:37 -0800

    sample app showing getting package info into repo

---
+++ MyProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using Spectre.Console;
+
+AnsiConsole.MarkupLine("Hello, :globe_showing_europe_africa:!");
+AnsiConsole.MarkupLine("This project uses the following packages");
+var table = new Spectre.Console.Table();
+table.AddColumns("Package", "Version");
+foreach(var package in Assembly.GetExecutingAssembly().GetCustomAttributes<AssemblyMetadataAttribute>().Where(attr => attr.Key == "PackageReferenceInfo")) {
+    if (package.Value.Split('@', 2) is [var name, var version ]){
+        table.AddRow(Markup.FromInterpolated($"[green]{name}[/]"), Markup.FromInterpolated($"[blue]{version}[/]"));
+    }
+}
+AnsiConsole.Write(table);
